be87c0104d89cb96b5455acefec2464f83c3b238
Diogo Duarte <costadiogo98@outlook.com>
2017-11-17 10:31:33 -0400

    initialization block

--- JAVA-PRATICAS/src/staticmodifier/classes/Car.java
+++ JAVA-PRATICAS/src/staticmodifier/classes/Car.java
@@ -3,13 +3,13 @@ package staticmodifier.classes;
  * Date 16/11/2017
  * @autor Diogo Duarte
  * Practicing static modifier
+ * modifi
  */
 public class Car {
     //limit speed should be 240km/h
     private String name;
     private double maxSpeed;
     public static double limitSpeed = 240;
-
     public Car(String name, double maxSpeed) {
         this.name = name;
         this.maxSpeed = maxSpeed;
@@ -20,6 +20,8 @@ public class Car {
         System.out.println("Max speed: "+this.maxSpeed);
         System.out.println("Limit speed: "+this.limitSpeed);
     }
+
+
     public String getName() {
         return name;
     }
