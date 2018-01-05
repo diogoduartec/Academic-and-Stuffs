521a82121cc9ee72f7af5c2e7fa0dd0d5a26f9b3
Diogo Duarte <costadiogo98@outlook.com>
2018-01-05 09:33:25 -0400

    exception and finaly

--- JAVA-PRATICAS/src/exception/checkedexception/test/CheckedExceptionTest.java
+++ JAVA-PRATICAS/src/exception/checkedexception/test/CheckedExceptionTest.java
@@ -5,11 +5,7 @@ import java.io.IOException;
 
 public class CheckedExceptionTest {
     public static void main(String[] args) {
-        try {
-            createFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        openFile();
     }
     public static void createFile() throws IOException{
         File file = new File("test.txt");
@@ -19,6 +15,20 @@ public class CheckedExceptionTest {
         } catch (IOException e){
             e.printStackTrace();
             throw e;
+        }
+    }
+    public static void openFile(){
+        File file = new File("test.txt");
+        try{
+            System.out.println("Loading file");
+            System.out.println("Running file");
+            System.out.println("Write file");
+
+        } catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        } finally {
+            System.out.println("Finish");
         }
     }
 }
