72b7efe721d6b0f71fb5585360bad11c74e8b0d9
Diogo Duarte <costadiogo98@outlook.com>
2018-01-05 10:48:01 -0400

    exception and try with resources

--- JAVA-PRATICAS/src/exception/rutimeexception/test/MultipleRuntimeExceptionTest.java
+++ JAVA-PRATICAS/src/exception/rutimeexception/test/MultipleRuntimeExceptionTest.java
@@ -1,23 +1,27 @@
 package exception.rutimeexception.test;
 
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
 /**
  * Create by Diogo Duarte in 05/01/2018
  * practicing Multiple Runtime Exception in java 6
  */
 public class MultipleRuntimeExceptionTest {
-    public static void main(String... args){
+    public static void main(String... args) {
         try {
             throw new IndexOutOfBoundsException();
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Array index of bounds exception");
-        } catch (IndexOutOfBoundsException e){
-            System.out.println("Index of bounds exception");
-        } catch (IllegalArgumentException e){
-            System.out.println("Illegal argument exception e");
-        } catch (ArithmeticException e){
-            System.out.println("Arithmetic exception");
-        } catch (RuntimeException e){
-            System.out.println("Runtime exception");
+        } catch (IndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Not possible");
         }
+        try {
+            maybeThrowException();
+        } catch(Exception e){
+            System.out.println("Not possible");
+        }
+    }
+
+    private static void maybeThrowException() throws SQLException, AWTException, FileNotFoundException{
     }
 }
