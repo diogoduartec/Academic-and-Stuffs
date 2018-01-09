5cbc87295e5380c0ec0d5f3c77ace4a481a8fd93
Diogo Duarte <costadiogo98@outlook.com>
2018-01-09 10:50:29 -0400

    Date, Calendar, DateFormat and Locale

---
+++ JAVA-PRATICAS/src/date/test/DateFormatTest.java
@@ -0,0 +1,19 @@
+package date.test;
+
+import java.util.Calendar;
+import java.text.DateFormat;
+public class DateFormatTest {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        DateFormat[] dateFormats = new DateFormat[6];
+        dateFormats[0] = DateFormat.getInstance();
+        dateFormats[1] = DateFormat.getDateInstance();
+        dateFormats[2] = DateFormat.getDateInstance(DateFormat.SHORT);
+        dateFormats[3] = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        dateFormats[4] = DateFormat.getDateInstance(DateFormat.LONG);
+        dateFormats[5] = DateFormat.getDateInstance(DateFormat.FULL);
+        for(DateFormat df : dateFormats){
+            System.out.println(df.format(c.getTime()));
+        }
+    }
+}
--- JAVA-PRATICAS/src/date/test/DateManipulationTest.java
+++ JAVA-PRATICAS/src/date/test/DateManipulationTest.java
@@ -1,8 +1,17 @@
 package date.test;
+import java.util.Calendar;
 import java.util.Date;
 public class DateManipulationTest {
     public static void main(String[] args) {
         Date date = new Date(1_000_000_000L);
         System.out.println(date);
+        date = new Date();
+        System.out.println(date);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        System.out.println(c.get(Calendar.HOUR));
+        c.add(Calendar.HOUR, 2);
+        System.out.println(Calendar.HOUR);
     }
 }
