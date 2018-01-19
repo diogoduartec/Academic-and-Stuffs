a796534b39e0e126cff81400bf31c078630882ba
Diogo Duarte <costadiogo98@outlook.com>
2018-01-19 16:22:37 -0400

    introduction

---
+++ JavaScript/javascript/03_events.js
@@ -0,0 +1,12 @@
+function sad(){
+	document.getElementById("nicolas").src = "https://i.imgur.com/cEaNF4i.jpg";
+}
+function happy(){
+	document.getElementById("nicolas").src = "https://i.imgur.com/hfU3Ddc.jpg";
+}
+function ungry(){
+	document.getElementById("nicolas").src = "https://i.imgur.com/dokeMhs.jpg";
+}
+function funny(){
+	document.getElementById("nicolas").src = "https://i.imgur.com/U9au72D.jpg";
+}
---
+++ JavaScript/javascript/04_variables.js
@@ -0,0 +1,9 @@
+var name = "Diogo"; //string
+var age = 19; //number
+var maried = true; //boolean
+const hight: 1.78
+function print(){
+	alert(name);
+	alert(age);
+	maried(maried);
+}
