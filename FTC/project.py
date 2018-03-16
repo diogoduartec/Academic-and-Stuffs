1e1ca019fd3b0b7cb8f9de0082abc92db82d9798
Diogo Duarte <duartediogo98@gmail.com>
2018-03-16 15:36:27 -0400

    modularization

--- FTC/project.py
+++ FTC/project.py
@@ -2,49 +2,48 @@ import re
 
 ## CONSTANTS VALUES
 CPF_SIZE = 14
-CNPJ_SIZE = 
-def isValidCpf(cpf): ##algorimo verificador de cpf
-    if len(cpf) != 14:
-        return 'False'
+CNPJ_SIZE = 18
+multCpf1 = [10,9,8,7,6,5,4,3,2]
+multCpf2 = [11,10,9,8,7,6,5,4,3,2]
+DIVISOR = 11
+## AUXILIAR METHODS
+def mult-sumArray(array, mult){
+	ac = 0
+	for i range(len(mult)):
+		ac += array[i]*mult[i]
+	return ac
+}
 
+def getDigit(int x):
+    if x%11 < 2:
+        return 0
+    else:
+        return 11 - ac%11
+
+##VALIDATION METHODS
+def isValidCpf(cpf):
+    if len(cpf) != CPF_SIZE:
+        return False
+        
     cpf=cpf.replace('.','')
     cpf=cpf.replace('-','')
+
+    ac = mult-sumArray(cpf,multCpf1)
+ 
+    if(getDigit(ac) != cpf[9]):
+        return False
+
+    ac = mult-sumArray(array, multCpf2)
     
-    mult = [11,10,9,8,7,6,5,4,3,2]
-    ac = 0
+    if(getDigit(ac) != cpf[10]):
+        return False
 
-    for i in range (1-10):
-        ac += int(mult[i])*int(cpf[i-1])
-
-    if ac%11 < 2:
-        digit = 0
-    else:
-        digit = 11 - ac%11
-
-    print(ac)
-    
-    if(digit != cpf[9]):
-        return 'False'
-
-    ac = 0
-    for i in range (0-10):
-        ac += int(mult[i])*int(cpf[i])
-        
-    if ac%11 < 2:
-        digit = 0
-    else:
-        digit = 11 - ac%11
-
-    ##print(digit)
-    
-    if(digit != cpf[10]):
-        return 'False'
-
-    return 'True'
+    return True
 
 def isValidCnpj(cnpj):
-	if len(cnpj) != 14:
-		
+	if len(cnpj) != CNPJ_SIZE:
+		return 'False'
 
 input_ = input()
 #match = re.search(r'\d{3}.\d{3}.\d{3}-\d{2}', input_) ##regex
+
