d51a16b650b19ccafb6fd41207f496771f4621a5
Diogo Duarte <costadiogo98@outlook.com>
2018-05-10 10:19:43 -0400

    commenting

--- (College) Operacional System/producer_consumer_problem/consumer.c
+++ (College) Operacional System/producer_consumer_problem/consumer.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
+#include <fcntl.h> //chamada de sistema que realiza operações de controle em um descritor de arquivo
+#include <sys/shm.h> //facilitador de memória compartilhada
+#include <sys/stat.h> //define a estrutura dos dados retornados pelas funções fstat(), lstat() e stat().
 
 int main(){
 	const int SIZE = 4096;//o tamanho (em bytes) do objeto de memória compartilhada
@@ -15,7 +15,7 @@ int main(){
 
 	shm_fd = shm_open(name, O_RDONLY, 0666);//abre o objeto de memória compartilhada
 
-	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);mapeia o objeto de memória compartilhada para a memória
+	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);//mapeia o objeto de memória compartilhada para a memória
 
 	printf("%s", (char *)ptr);//lê a partir do objeto de memória compartilhada
 
